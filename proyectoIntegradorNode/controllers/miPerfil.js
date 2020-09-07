3463b97d343d355e1ee9373a17f1f66fe730ea00
Mateo Vidal <matevidal@hotmail.com.ar>
2020-09-06 23:01:56 -0300

    varios

---
+++ proyectoIntegradorNode/controllers/miPerfil.js
@@ -0,0 +1,51 @@
+var miPerfil = require("../posts/post.js")
+var miPerfilController = { 
+
+index:function (req,res){
+    // aca escribo que pasa cuando el usuario quiere agregar un post
+        res.send (miPerfil.lista)
+    },
+marca: function (req,res){
+    // nombre var = lo que recupero
+    var queMarca = req.params.queMarca    
+
+   let resultadoBusqueda = miPerfil.porMarca(queMarca)
+
+    if (resultadoBusqueda.length == 0) {
+        res.send("no encontre nada")
+    }else{
+        res.send(resultadoBusqueda)
+    }
+    },
+
+color: function (req,res){
+        // nombre var = lo que recupero
+        var queColor = req.params.queColor   
+    
+       let resultadoBusqueda = headerDeslogueado.porColor(queColor)
+    
+        if (resultadoBusqueda.length == 0) {
+            res.send("no encontre nada")
+        }else{
+            res.send(resultadoBusqueda)
+        }
+        },
+anio:  function (req,res){
+    // nombre var = lo que recupero
+    var queAnio = req.params.queAnio   
+
+   let resultadoBusqueda = headerDeslogueado.porAnio(queAnio)
+
+    if (resultadoBusqueda.length == 0) {
+        res.send("no encontre nada")
+    }else{
+        res.send(resultadoBusqueda)
+    }
+    }
+
+
+
+
+
+};
+module.exports = headerDeslogueadoController;
---
+++ proyectoIntegradorNode/controllers/resultadoBusqueda.js
@@ -0,0 +1,51 @@
+var resultadoBusqueda = require("../posts/post.js")
+var resultadoBusquedaController = { 
+
+index:function (req,res){
+    // aca escribo que pasa cuando el usuario quiere agregar un post
+        res.send (resultadoBusqueda.lista)
+    },
+marca: function (req,res){
+    // nombre var = lo que recupero
+    var queMarca = req.params.queMarca    
+
+   let resultadoBusqueda = resultadoBusqueda.porMarca(queMarca)
+
+    if (resultadoBusqueda.length == 0) {
+        res.send("no encontre nada")
+    }else{
+        res.send(resultadoBusqueda)
+    }
+    },
+
+color: function (req,res){
+        // nombre var = lo que recupero
+        var queColor = req.params.queColor   
+    
+       let resultadoBusqueda = headerDeslogueado.porColor(queColor)
+    
+        if (resultadoBusqueda.length == 0) {
+            res.send("no encontre nada")
+        }else{
+            res.send(resultadoBusqueda)
+        }
+        },
+anio:  function (req,res){
+    // nombre var = lo que recupero
+    var queAnio = req.params.queAnio   
+
+   let resultadoBusqueda = headerDeslogueado.porAnio(queAnio)
+
+    if (resultadoBusqueda.length == 0) {
+        res.send("no encontre nada")
+    }else{
+        res.send(resultadoBusqueda)
+    }
+    }
+
+
+
+
+
+};
+module.exports = headerDeslogueadoController;
