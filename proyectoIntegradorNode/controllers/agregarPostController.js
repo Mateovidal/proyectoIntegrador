13a51a79b1a407277cdfd072c6946baab6868c83
Ignacio Estivill <iestivill@udesa.edu.ar>
2020-09-08 23:57:15 -0300

    controllers renombrados

--- proyectoIntegradorNode/controllers/agregarPostController.js
+++ proyectoIntegradorNode/controllers/agregarPostController.js
@@ -1,47 +1,49 @@
 var post = require("../posts/post.js")
 var agregarPostController = { 
+detalle: function (post,res){ 
 
-index:function (req,res){
-    // aca escribo que pasa cuando el usuario quiere agregar un post
-        res.send (post.lista)
-    },
-marca: function (req,res){
-    // nombre var = lo que recupero
-    var queMarca = req.params.queMarca    
+}
+// index:function (req,res){
+//     // aca escribo que pasa cuando el usuario quiere agregar un post
+//         res.send (post.lista)
+//     },
+// marca: function (req,res){
+//     // nombre var = lo que recupero
+//     var queMarca = req.params.queMarca    
 
-   let resultadoBusqueda = post.porMarca(queMarca)
+//    let resultadoBusqueda = post.porMarca(queMarca)
 
-    if (resultadoBusqueda.length == 0) {
-        res.send("no encontre nada")
-    }else{
-        res.send(resultadoBusqueda)
-    }
-    },
+//     if (resultadoBusqueda.length == 0) {
+//         res.send("no encontre nada")
+//     }else{
+//         res.send(resultadoBusqueda)
+//     }
+//     },
 
-color: function (req,res){
-        // nombre var = lo que recupero
-        var queColor = req.params.queColor   
+// color: function (req,res){
+//         // nombre var = lo que recupero
+//         var queColor = req.params.queColor   
     
-       let resultadoBusqueda = post.porColor(queColor)
+//        let resultadoBusqueda = post.porColor(queColor)
     
-        if (resultadoBusqueda.length == 0) {
-            res.send("no encontre nada")
-        }else{
-            res.send(resultadoBusqueda)
-        }
-        },
-anio:  function (req,res){
-    // nombre var = lo que recupero
-    var queAnio = req.params.queAnio   
+//         if (resultadoBusqueda.length == 0) {
+//             res.send("no encontre nada")
+//         }else{
+//             res.send(resultadoBusqueda)
+//         }
+//         },
+// anio:  function (req,res){
+//     // nombre var = lo que recupero
+//     var queAnio = req.params.queAnio   
 
-   let resultadoBusqueda = post.porAnio(queAnio)
+//    let resultadoBusqueda = post.porAnio(queAnio)
 
-    if (resultadoBusqueda.length == 0) {
-        res.send("no encontre nada")
-    }else{
-        res.send(resultadoBusqueda)
-    }
-    }
+//     if (resultadoBusqueda.length == 0) {
+//         res.send("no encontre nada")
+//     }else{
+//         res.send(resultadoBusqueda)
+//     }
+//     }
 
 
 
