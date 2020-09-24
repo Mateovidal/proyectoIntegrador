d916adac07aff43a64956d62f449a96c64e237e4
Mateo Vidal <matevidal@hotmail.com.ar>
2020-09-24 18:53:19 -0300

    arreglitos finales

--- proyectoIntegradorNode/routes/detallePost.js
+++ proyectoIntegradorNode/routes/detallePost.js
@@ -1,16 +1,3 @@
-// var express = require('express');
-// var router = express.Router();
-
-
-// router.get("/detallePost" , function(req,res) {
-
-
-// res.send ("Detalle post");
-
-// });
-
-// module.exports = router;
-
 var express = require('express');
 var router = express.Router();
 
