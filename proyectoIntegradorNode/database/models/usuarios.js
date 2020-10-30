46cfad5437fa24b979ebc93afb2b8c2d5a9c3fc6
Mateo Vidal <matevidal@hotmail.com.ar>
2020-10-30 20:35:58 -0300

    jhsjk

--- proyectoIntegradorNode/database/models/usuarios.js
+++ proyectoIntegradorNode/database/models/usuarios.js
@@ -10,7 +10,7 @@ module.exports = (sequelize,DataTypes) => {
         fecha:{
             type:DataTypes.DATE
         },
-        name: {
+        username: {
             type: DataTypes.STRING
 
         },
